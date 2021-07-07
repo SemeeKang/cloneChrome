dfa21513fc67ff0d3cfc28311a7ea76c1fd3b4e6
Semee Kang <semee.kang01@gmail.com>
2021-07-07 12:09:48 -0400

    create clock

---
+++ js/clock.js
@@ -0,0 +1,18 @@
+const clock = document.querySelector("h2#clock");
+
+
+
+
+function getClcok() {
+    const date = new Date();
+
+    const hour = String(date.getHours()).padStart(2, "0");
+    const minute = String(date.getMinutes()).padStart(2, "0");
+    const second = String(date.getSeconds()).padStart(2, "0");
+    
+    clock.innerHTML = `${hour}:${minute}:${second}`;
+}
+
+
+getClcok();
+setInterval(getClcok, 1000);
