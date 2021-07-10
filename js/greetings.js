29d2887170be7286f14f0651a38505582c2cba31
Semee Kang <semee.kang01@gmail.com>
2021-07-09 23:57:27 -0400

    create login form

--- js/greetings.js
+++ js/greetings.js
@@ -1,1 +1,12 @@
-//handling login
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+
+function onSubmitHandler(e) {
+    e.preventDefault();
+
+    const username = loginInput.value;
+    console.log(username);
+}
+
+loginForm.addEventListener("submit", onSubmitHandler)
