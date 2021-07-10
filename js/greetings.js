8d9f818c6576db431cbbd672f83957f699e1b10b
Semee Kang <semee.kang01@gmail.com>
2021-07-10 00:41:49 -0400

    save username in local storage

--- js/greetings.js
+++ js/greetings.js
@@ -1,12 +1,30 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
+const HIDDEN_CLASSNAME = "hidden";
+const STORAGE_KEY_USERNAME = "username";
 
 function onSubmitHandler(e) {
     e.preventDefault();
 
     const username = loginInput.value;
-    console.log(username);
+    localStorage.setItem(STORAGE_KEY_USERNAME, username);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onSubmitHandler)
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(STORAGE_KEY_USERNAME);
+if(savedUsername === null) { 
+    // show form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onSubmitHandler);
+} else {
+    paintGreetings(savedUsername);
+}
+
